51d46de94d16de9db3b70a2c5f3f885bf1511867
Stephan Scheying <229470+scheying@users.noreply.github.com>
2021-09-24 12:42:58 +0200

    Update gcloud-auth-wrapper.sh

--- gcloud-auth-wrapper.sh
+++ gcloud-auth-wrapper.sh
@@ -3,6 +3,8 @@ set -e
 
 b="$(basename -- $0)"
 
+export KUBECONFIG="/tmp/.kube/config"
+
 if [ "$b" = "kubectl" ] && [ ! -e ~/.kube/config ] || [ ! -e ~/.config/gcloud/application_default_credentials.json ]; then
 
   [ -z "$PLUGIN_ZONE" ] && echo "Need to set 'zone'" && exit 1;
@@ -13,14 +15,14 @@ if [ "$b" = "kubectl" ] && [ ! -e ~/.kube/config ] || [ ! -e ~/.config/gcloud/ap
   echo "Generating K8s credentials..."
 
   # Use standalone 'echo' to be able to suppress backslash-escape interpretation
-  /bin/echo -E ${PLUGIN_GCP_CREDENTIALS} > credentials.json
+  /bin/echo -E ${PLUGIN_GCP_CREDENTIALS} > /tmp/credentials.json
   
-  /usr/bin/gcloud.original auth activate-service-account --key-file=credentials.json
+  /usr/bin/gcloud.original auth activate-service-account --key-file=/tmp/credentials.json
 
   if [ "$b" = "kubectl" ]; then
     /usr/bin/gcloud.original container clusters get-credentials ${PLUGIN_CLUSTER} --project=${PLUGIN_PROJECT} --zone=${PLUGIN_ZONE}
 
-    echo "... credentials written to ~/.kube/config"
+    echo "... credentials written to ${KUBECONFIG}"
 
     if [ -n "$PLUGIN_NAMESPACE" ]; then
       kubectl config set-context --current --namespace=${PLUGIN_NAMESPACE}
