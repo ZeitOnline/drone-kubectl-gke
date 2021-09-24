abe81f103a726dfa042e257af40f78b49124691b
Stephan Scheying <stephan@scheying.de>
2021-09-24 15:51:09 +0200

    Clean up auth wrapper script

--- gcloud-auth-wrapper.sh
+++ gcloud-auth-wrapper.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 set -e
 
-b="$(basename -- $0)"
+command="$(basename -- $0)"
 
+# Write credentials to a location outside the Drone workspace to
+# avoid collisions when steps run in parallel
 export KUBECONFIG="/tmp/.kube/config"
 export CLOUDSDK_CONFIG="/tmp/gcloud/"
 
-if [ "$b" = "kubectl" ] && [ ! -e "${KUBECONFIG}" ] || [ ! -e "${CLOUDSDK_CONFIG}/active_config" ]; then
+if [ ! -e CLOUDSDK_CONFIG ]; then
 
   [ -z "$PLUGIN_ZONE" ] && echo "Need to set 'zone'" && exit 1;
   [ -z "$PLUGIN_PROJECT" ] && echo "Need to set 'project'" && exit 1;
   [ -z "$PLUGIN_CLUSTER" ] && echo "Need to set 'cluster'" && exit 1;
   [ -z "$PLUGIN_GCP_CREDENTIALS" ] && echo "Need to set 'gcp_credentials'" && exit 1;
 
-  echo "Generating K8s credentials..."
+  echo "Activating GCP service account..."
 
   # Use standalone 'echo' to be able to suppress backslash-escape interpretation
   /bin/echo -E ${PLUGIN_GCP_CREDENTIALS} > /tmp/credentials.json
   
   /usr/bin/gcloud.original auth activate-service-account --key-file=/tmp/credentials.json
+fi
 
-  if [ "$b" = "kubectl" ]; then
-    /usr/bin/gcloud.original container clusters get-credentials ${PLUGIN_CLUSTER} --project=${PLUGIN_PROJECT} --zone=${PLUGIN_ZONE}
+if [ $command = "kubectl" ] && [ ! -e $KUBECONFIG ]; then
+  echo "Writing k8s credentials to ${KUBECONFIG}..."
 
-    echo "... credentials written to ${KUBECONFIG}"
+  /usr/bin/gcloud.original container clusters get-credentials ${PLUGIN_CLUSTER} --project=${PLUGIN_PROJECT} --zone=${PLUGIN_ZONE}
 
-    if [ -n "$PLUGIN_NAMESPACE" ]; then
-      kubectl.original config set-context --current --namespace=${PLUGIN_NAMESPACE}
-      echo "... namespace set to '${PLUGIN_NAMESPACE}'."
-    fi
+  if [ -n "$PLUGIN_NAMESPACE" ]; then
+    echo "Setting namespace of k8s context to '${PLUGIN_NAMESPACE}'..."
+
+    kubectl.original config set-context --current --namespace=${PLUGIN_NAMESPACE}
   fi
 fi
 
